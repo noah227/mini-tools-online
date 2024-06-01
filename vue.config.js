db6a9c0452675eb20be74368dad2f21a6f3d6361
NoahYoung <noah227@foxmail.com>
2024-06-01 20:18:39 +0800

    build: 使用相对路径构建

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const {defineConfig} = require('@vue/cli-service')
 const webpack = require("webpack")
 module.exports = defineConfig({
     transpileDependencies: true,
+    publicPath: "./",
     configureWebpack: {
         plugins: [
             new webpack.DefinePlugin({
