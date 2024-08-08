6ddd01341e6af87362c73e7d122b5712788afe42
noah227 <noah227@foxmail.com>
2024-08-08 16:21:49 +0800

    chore: 欢迎信息输出顺序优化

--- src/main.extra.ts
+++ src/main.extra.ts
@@ -1,6 +1,6 @@
 import console from "@cynario/console.image"
 const pkg = require("/package.json")
 
-console.log("欢迎使用！")
-console.image(require("@/assets/hi.png"))
-console.log(`${location.origin} v${pkg.version}`)
+console.image(require("@/assets/hi.png")).then(() => {
+    console.log(`${location.origin} v${pkg.version}`)
+})
