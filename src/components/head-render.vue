85fa05db306103be6dd3897d401df9e5ab186607
noah227 <noah227@foxmail.com>
2024-10-09 10:21:02 +0800

    chore: 补充组件调整优化

--- src/components/head-render.vue
+++ src/components/head-render.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="common-title" :title="renderBindTitle">
+    <div class="common-title-wrapper" :title="renderBindTitle">
         <slot name="title-before"></slot>
-        {{ renderTitle }}
+        <span class="common-title">{{ renderTitle }}</span>
         <slot name="title-after"></slot>
     </div>
 </template>
@@ -16,12 +16,17 @@ const renderBindTitle = computed(() => route.meta.description as any)
 </script>
 
 <style lang="scss" scoped>
-.common-title {
-    font-size: 1.17em;
-    font-weight: bold;
+.common-title-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
     padding-block-start: 1em;
     padding-block-end: 1em;
-    user-select: none;
-    text-align: center;
+
+    > .common-title {
+        font-size: 1.17em;
+        font-weight: bold;
+        user-select: none;
+    }
 }
 </style>
