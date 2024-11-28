e0c7bb5b5d717e51e43075a3941f8316e21ba68d
noah227 <noah227@foxmail.com>
2024-11-28 13:15:14 +0800

    feat(json-highlight): 支持复制按钮；同时修复了渲染格式错误（异常的缩进）

--- src/components/json-highlight.vue
+++ src/components/json-highlight.vue
@@ -1,17 +1,32 @@
 <template>
-    <pre class="json-highlight"><code ref="refCode" class="language-json"></code></pre>
+    <pre class="json-highlight"><code ref="refCode" class="language-json"
+                                      data-prismjs-copy="复制" data-prismjs-copy-error="复制失败"
+                                      data-prismjs-copy-success="已复制"
+    ></code></pre>
 </template>
 
 <script lang="ts" setup>
 import {nextTick, onMounted, ref, watch} from "vue";
 import "prismjs/themes/prism-coy.min.css"
+import "prismjs/plugins/toolbar/prism-toolbar.min.css"
 
 const prismJs = require("prismjs")
+
+const pluginInitialized = ref(false)
+const initPluginRequirement = () => {
+    if (pluginInitialized.value) return
+    require("prismjs/plugins/toolbar/prism-toolbar.js")
+    require("prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js")
+    pluginInitialized.value = true
+}
 
 const props = defineProps({
     code: {
         type: String,
         required: true
+    },
+    enableCopy: {
+        type: Boolean
     }
 })
 
@@ -29,6 +44,7 @@ watch(() => props.code, () => {
 })
 
 onMounted(() => {
+    props.enableCopy && initPluginRequirement()
     update()
 })
 </script>
